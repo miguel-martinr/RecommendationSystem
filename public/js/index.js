a301d08f9423a4ad922c6dbd47336812f03c9539
Miguel Martín <alu0101209777@ull.edu.es>
2021-11-20 23:27:57 +0000

    gui added

--- public/js/index.js
+++ public/js/index.js
@@ -1,133 +1,35 @@
-console.log("hello")
+import { Recommender } from "./recommender.js";
 
+const recommender = new Recommender();
+
+
+
+// Read matrix
 let reader = new FileReader()
 
+reader.onload = () => {
+  recommender.setUtilityMatrix(reader.result);
 
-class Recommender {
-  constructor() {
+  const formattedMatrix = Recommender.formatMatrix(recommender.utility_matrix);
+  
+  const matrixContainer = document.getElementById('matrix_container');
+  matrixContainer.innerHTML = "";
 
-    reader.onload = () => {
-      recommender.set_utility_matrix(reader.result);
-    }
-
-    this.sim = this.pearson;
-  }
-
-  set_utility_matrix(matrix) {
-    const not_value = "-";;
-    this.utility_matrix = matrix
-      .split("\n")
-      .map((str_row) => str_row.split(" "))
-      .map((row) => row.map((char) => char == not_value ? undefined : parseInt(char))
-        .filter((calif) => calif === undefined || !isNaN(calif)));
-  }
-
-  print() {
-    let content = "\nRecomendador:\n";
-
-    content += this.utility_matrix.map((row) => row.map(item => item === undefined ? "-" : item.toString()));
-    content += "\n";
-
-    console.log(content);
-  }
-
-  pearson(u, v) {
-
-    // SUM [(r(u, i) - mean(u)) * (r(v, i) - mean(v))]
-    let A = 0;
-
-    // Cx = SUM[(r(x, i) - mean(x))^2]
-    let Bu = 0;
-    let Bv = 0;
-
-    const u_mean = this.getUserMean(u);
-    const v_mean = this.getUserMean(v);
-
-    for (let i = 0; i < this.utility_matrix[0].length; i++) {
-
-      // r(u, i)
-      const r_u = this.utility_matrix[u][i];
-      // r(v, i)
-      const r_v = this.utility_matrix[v][i];
-
-      // Work only on Suv set
-      if (r_u === undefined || r_v === undefined) continue;
-
-      const u_diff = r_u - u_mean;
-      const v_diff = r_v - v_mean;
-
-      A += u_diff * v_diff;
-
-      Bu += u_diff ** 2;
-      Bv += v_diff ** 2;
-    }
-
-
-    return A / Math.sqrt(Bu * Bv);
-  }
-
-  cosine(u, v) {
-
-    // SUM[r(u, i) * r(v, i)]
-    let A = 0;
-
-    // Bx = SUM[r(x, i) ^ 2]
-    let Bu = 0;
-    let Bv = 0;
-
-    for (let i = 0; i < this.utility_matrix[0].length; i++) {
-      // r(u, i)
-      const r_u = this.utility_matrix[u][i];
-      // r(v, i)
-      const r_v = this.utility_matrix[v][i];
-
-      // Work only on Suv set
-      if (r_u === undefined || r_v === undefined) continue;
-
-      A += r_u * r_v;
-      Bu += r_u ** 2;
-      Bv += r_v ** 2;
-    }
-
-    return A / Math.sqrt(Bu * Bv);
-  }
-
-  euclidean(u, v) {
-    let A = 0;
-    for (let i = 0; i < this.utility_matrix[0].length; i++) {
-      // r(u, i)
-      const r_u = this.utility_matrix[u][i];
-      // r(v, i)
-      const r_v = this.utility_matrix[v][i];
-
-      // Work only on Suv set
-      if (r_u === undefined || r_v === undefined) continue;
-
-      A += (r_u - r_v) ** 2
-    }
-
-    return Math.sqrt(A);
-  }
-
-  getUserMean(user_index) {
-    const user_califications = this.utility_matrix[user_index];
-
-    if (!user_califications) throw new Error(`There's not an user at index ${user_index}`);
-
-    let num_of_califications = 0;
-    const califications_sum = user_califications.reduce((sum, calif) => {
-      if (calif === undefined) return sum;
-
-      num_of_califications++;
-      return sum + calif;
-    }, 0);
-
-    return califications_sum / num_of_califications;
-  }
+  
+  // Remove previous calculated matrix
+  document.getElementById('matrix-two-title').hidden = true;
+  const newMatrixContainer = document.getElementById('new_matrix_container');
+  newMatrixContainer.innerHTML = "";
+  
+  // Show new original matrix
+  const opts = { 
+    markedCells: recommender.emptyItems, 
+    styleClasses: ["text-white", "bg-danger"] 
+  };
+  document.getElementById('matrix-one-title').hidden = false;
+  matrixContainer.appendChild(createTable(formattedMatrix, opts));
 }
 
-
-// Leer matriz 
 const readMatrix = (input_event) => {
   const { target } = input_event;
 
@@ -135,20 +37,106 @@ const readMatrix = (input_event) => {
     alert("Debes subir 1 matriz de utilidad");
   }
 
-  file = file_input.files[0];
+  const file = file_input.files[0];
   reader.readAsText(file);
+}
+
+const file_input = document.getElementById('utility_matrix_input');
+file_input.addEventListener('input', readMatrix);
+
+
+
+
+// Change similarity metric
+document.getElementById('metrics_dropdown').addEventListener('change', (ev) => {
+  recommender.setMetric(ev.target.value);
+});
+
+// Change prediction method
+document.getElementById('prediction_method').addEventListener('change', (ev) => {
+  recommender.setPredictor(ev.target.value);
+});
+
+
+// Calculate new matrix
+document.getElementById('calc_form').addEventListener('submit', (ev) => {
+  ev.preventDefault();
+  const numOfNeighbors = parseInt(document.getElementById('neighbors_number_input').value);
+  recommender.numOfNeighbors = numOfNeighbors;
+
+  const newMatrix = recommender.calculate();
+  const formattedMatrix = Recommender.formatMatrix(newMatrix);
+
+  const newMatrixContainer = document.getElementById('new_matrix_container');
+  newMatrixContainer.innerHTML = "";
+
+  const opts = { 
+    markedCells: recommender.emptyItems, 
+    styleClasses: ["text-white", "bg-success"] 
+  };
+
+  document.getElementById('matrix-two-title').hidden = false;
+  newMatrixContainer.appendChild(createTable(formattedMatrix, opts));
+});
+
+
+
+
+const createTable = (matrix, opts) => {
+
+  const { markedCells, styleClasses } = opts;
+
+  const table = document.createElement('table');
+  table.classList.add('table');
+  const thead = document.createElement('thead');
+
+  thead.innerHTML = '<th scope="col">#</th>';
+
+  matrix[0].forEach((_, i) => thead.innerHTML += `<th scope="col">Item ${i + 1}</th>`);
+  table.appendChild(createTr(thead.innerHTML));
+
+
+
+  const tbody = document.createElement('tbody');
+
+  const body = matrix.map((row, i) => {
+    return row.map((item, j) => {
+      const td = document.createElement('td');
+      td.innerHTML = item;
+      return td;
+    });
+  });
+
+  markedCells.forEach(([i, j]) => {
+    body[i][j].classList.add(...styleClasses);
+  });
+
+  body.forEach((row, i) => {
+    const rowHeader = `<th scope="row">User ${i + 1}</th>`;
+    const tr = document.createElement('tr');
+    tr.innerHTML = rowHeader;
+    row.forEach((_, j) => {
+      tr.appendChild(body[i][j]);
+      tbody.appendChild(tr);
+    });
+  });
+
+  table.appendChild(tbody);
+  return table;
+}
+
+const createTr = (innerHTML) => {
+  const tr = document.createElement('tr');
+  tr.innerHTML = innerHTML;
+  return tr;
 }
 
 
 
-const recommender = new Recommender();
 
 
 
 
-
-const file_input = document.getElementById('utility_matrix_input');
-file_input.addEventListener('input', readMatrix);
 
 
 
---
+++ public/js/recommender.js
@@ -0,0 +1,259 @@
+const sorters = {
+  increscent: (a, b) => a - b,
+  decrescent: (a, b) => b - a
+}
+
+const predictors = {
+  collaborativeFiltering: {
+    userBased: {
+      simple: "userBased/collaborativeFiltering/simple",
+      meanDiff: "userBased/collaborativeFiltering/meanDiff"
+    }
+  }
+}
+
+const metrics = {
+  pearson: "pearson",
+  cosine: "cosine",
+  euclidean: "euclidean"
+}
+
+
+
+export class Recommender {
+  constructor() {
+    this.sim = this.pearson;
+    this.numOfNeighbors = 3;
+
+    this.setMetric(metrics.pearson);
+    this.setPredictor(predictors.collaborativeFiltering.userBased.meanDiff);
+  }
+
+  setUtilityMatrix(matrix) {
+    const not_value = "-";;
+    this.utility_matrix = matrix
+      .split("\n")
+      .map((str_row) => str_row.split(" "))
+      .map((row) => row.map((char) => char == not_value ? undefined : parseInt(char))
+        .filter((calif) => calif === undefined || !isNaN(calif)));
+  }
+
+
+  get emptyItems() {
+    const positions = [];
+    this.utility_matrix.forEach((row, i) => {
+      row.forEach((item, j) => {
+        if (item === undefined) positions.push([i, j]);
+      });
+    });
+    return positions;
+  }
+
+  // Similarity calculators
+  similarityCalculators = {
+    pearson(u, v) {
+
+      // SUM [(r(u, i) - mean(u)) * (r(v, i) - mean(v))]
+      let A = 0;
+
+      // Cx = SUM[(r(x, i) - mean(x))^2]
+      let Bu = 0;
+      let Bv = 0;
+
+      const u_mean = this.getUserMean(u);
+      const v_mean = this.getUserMean(v);
+
+      for (let i = 0; i < this.utility_matrix[0].length; i++) {
+
+        // r(u, i)
+        const r_u = this.utility_matrix[u][i];
+        // r(v, i)
+        const r_v = this.utility_matrix[v][i];
+
+        // Work only on Suv set
+        if (r_u === undefined || r_v === undefined) continue;
+
+        const u_diff = r_u - u_mean;
+        const v_diff = r_v - v_mean;
+
+        A += u_diff * v_diff;
+
+        Bu += u_diff ** 2;
+        Bv += v_diff ** 2;
+      }
+
+
+      return A / Math.sqrt(Bu * Bv);
+    },
+
+    cosine(u, v) {
+
+      // SUM[r(u, i) * r(v, i)]
+      let A = 0;
+
+      // Bx = SUM[r(x, i) ^ 2]
+      let Bu = 0;
+      let Bv = 0;
+
+      for (let i = 0; i < this.utility_matrix[0].length; i++) {
+        // r(u, i)
+        const r_u = this.utility_matrix[u][i];
+        // r(v, i)
+        const r_v = this.utility_matrix[v][i];
+
+        // Work only on Suv set
+        if (r_u === undefined || r_v === undefined) continue;
+
+        A += r_u * r_v;
+        Bu += r_u ** 2;
+        Bv += r_v ** 2;
+      }
+
+      return A / Math.sqrt(Bu * Bv);
+    },
+
+    euclidean(u, v) {
+      let A = 0;
+      for (let i = 0; i < this.utility_matrix[0].length; i++) {
+        // r(u, i)
+        const r_u = this.utility_matrix[u][i];
+        // r(v, i)
+        const r_v = this.utility_matrix[v][i];
+
+        // Work only on Suv set
+        if (r_u === undefined || r_v === undefined) continue;
+
+        A += (r_u - r_v) ** 2
+      }
+
+      return Math.sqrt(A);
+    }
+  }
+
+
+  // Returns user_index mean of califications
+  getUserMean(user_index) {
+    const user_califications = this.utility_matrix[user_index];
+
+    if (!user_califications) throw new Error(`There's not any user at index ${user_index}`);
+
+    let num_of_califications = 0;
+    const califications_sum = user_califications.reduce((sum, calif) => {
+      if (calif === undefined) return sum;
+
+      num_of_califications++;
+      return sum + calif;
+    }, 0);
+
+    return califications_sum / num_of_califications;
+  }
+
+  // Gets k nearest neighbors to user_index user (according to sim function)
+  getNearestNeighbors(u_user, neighborsNum) {
+    const user_califications = this.utility_matrix[u_user];
+
+    if (!user_califications) throw new Error(`There's not any user at index ${u_user}`);
+
+    const users_similarity = this.utility_matrix.map((_, v_user) => ({ index: v_user, sim: this.sim(u_user, v_user) }));
+
+    return users_similarity.sort((a, b) => this.similaritySorter(a.sim, b.sim))
+      .slice(1, neighborsNum + 1); // Ignores similarity with u_user itself (1)
+  }
+
+
+  // Prediction methods
+  collaborativeFiltering = {
+    userBased: {
+      simple(u, i) {
+        const neighbors = this.getNearestNeighbors(u, this.numOfNeighbors);
+        // SUM[ sim(u, v) * r(v, i)]
+        let A = 0;
+        // SUM[ |sim(u, v)|]
+        let B = 0;
+
+        neighbors.forEach((neighbor) => {
+          const r_v = this.utility_matrix[neighbor.index][i];
+
+          if (r_v === undefined) return;
+
+          A += neighbor.sim * r_v;
+          B += Math.abs(neighbor.sim);
+        });
+
+        return A / B;
+      },
+
+      meanDiff(u, i) {
+        const neighbors = this.getNearestNeighbors(u, this.numOfNeighbors);
+        // SUM[ sim(u, v) * r(v, i)]
+        let A = 0;
+        // SUM[ |sim(u, v)|]
+        let B = 0;
+
+
+        neighbors.forEach((neighbor) => {
+          const r_v = this.utility_matrix[neighbor.index][i];
+
+          if (r_v === undefined) return;
+
+          A += neighbor.sim * (r_v - this.getUserMean(neighbor.index));
+          B += Math.abs(neighbor.sim);
+        });
+
+        const u_mean = this.getUserMean(u);
+        return u_mean + A / B;
+      }
+    }
+  }
+
+
+  // Setters
+  setMetric(metricName) {
+    switch (metricName) {
+      case metrics.pearson:
+        this.sim = this.similarityCalculators.pearson;
+        this.similaritySorter = sorters.decrescent;
+        return;
+      case metrics.cosine:
+        this.sim = this.similarityCalculators.cosine;
+        this.similaritySorter = sorters.decrescent;
+        return;
+      case metrics.euclidean:
+        this.sim = this.similarityCalculators.euclidean;
+        this.similaritySorter = sorters.increscent;
+
+      default:
+        throw new Error(`Unknown metric method: ${metricName}`);
+    }
+  }
+
+  setPredictor(predictorName) {
+    switch (predictorName) {
+      case predictors.collaborativeFiltering.userBased.simple:
+        this.predict = this.collaborativeFiltering.userBased.simple;
+        return;
+      case predictors.collaborativeFiltering.userBased.meanDiff:
+        this.predict = this.collaborativeFiltering.userBased.meanDiff;
+        return;
+      default:
+        throw new Error(`Unknown prediction method: ${predictorName}`);
+    }
+  }
+
+
+  // Calculate new full matrix
+  calculate() {
+    return this.utility_matrix
+      .map((califications, u) => califications
+        .map((calif, i) => calif !== undefined ? calif : this.predict(u, i)))
+  }
+
+
+  static printMatrix(matrix) {
+    return this.formatMatrix(matrix).map(row => row.join(" ")).join("\n");
+  }
+
+  static formatMatrix(matrix) {
+    return matrix.map((row) => row.map(val => val === undefined ? "-" : val.toFixed(2)));
+  }
+}
