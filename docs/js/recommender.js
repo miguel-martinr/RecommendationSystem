5cacf1910dbcb071d00eecc7b4e88ad6a65cdf26
Miguel Martín <alu0101209777@ull.edu.es>
2021-11-26 19:44:58 +0000

    neighbors method fixed

--- docs/js/recommender.js
+++ docs/js/recommender.js
@@ -167,7 +167,7 @@ export class Recommender {
 
 
   // Gets k nearest neighbors to userIndex user (according to sim function)
-  getNearestNeighbors(u, neighborsNum) {
+  getNearestNeighbors(u, neighborsNum, i) {
     const userCalifications = this.utilityMatrix[u];
 
     if (!userCalifications) throw new Error(`There's not any user at index ${u}`);
@@ -175,8 +175,18 @@ export class Recommender {
     const similarityMatrix = this.similarityMatrix;
     const usersSimilarity = similarityMatrix[u].map((sim, index) => ({ index, sim }));
 
-    return usersSimilarity.sort((a, b) => this.similaritySorter(a.sim, b.sim))
-      .slice(1, neighborsNum + 1); // Ignores similarity with u itself (1)
+    const sorted = usersSimilarity.sort((a, b) => this.similaritySorter(a.sim, b.sim))
+    .slice(1); // Ignores similarity with u itself (1)
+
+    let result = [];
+    let j = 0;
+    while (result.length < neighborsNum && j < sorted.length) {
+      const neighbor = sorted[j++];
+      if (this.utilityMatrix[neighbor.index][i] === undefined) continue;
+      result.push(neighbor);
+    }
+
+    return  result;
   }
 
 
@@ -184,7 +194,7 @@ export class Recommender {
   collaborativeFiltering = {
     userBased: {
       simple(u, i) {
-        const neighbors = this.getNearestNeighbors(u, this.numOfNeighbors);
+        const neighbors = this.getNearestNeighbors(u, this.numOfNeighbors, i);
         // SUM[ sim(u, v) * r(v, i)]
         let A = 0;
         // SUM[ |sim(u, v)|]
@@ -192,8 +202,6 @@ export class Recommender {
 
         neighbors.forEach((neighbor) => {
           const rV = this.utilityMatrix[neighbor.index][i];
-
-          if (rV === undefined) return;
 
           A += neighbor.sim * rV;
           B += Math.abs(neighbor.sim);
@@ -203,7 +211,7 @@ export class Recommender {
       },
 
       meanDiff(u, i) {
-        const neighbors = this.getNearestNeighbors(u, this.numOfNeighbors);
+        const neighbors = this.getNearestNeighbors(u, this.numOfNeighbors, i);
         // SUM[ sim(u, v) * r(v, i)]
         let A = 0;
         // SUM[ |sim(u, v)|]
